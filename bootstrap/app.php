7921d5edec4402dd2bb6bb8b4e1b941ddc667bb8
Hager Abdo <hager.abdo121@hotmail.com>
2025-05-19 01:59:17 +0300

    feat(scheduler): add scheduled email sending for notes
    Introduce a new command `SendScheduledNotes` to send emails for published notes at a specific time. The command is scheduled to run daily at 08:00 AM. Additionally, a new job `SendEmail` is created to handle the email sending functionality. This ensures that users receive their notes promptly at the scheduled time.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,15 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('app:send-scheduled-notes')->timezone('Africa/')->dailyAt('08:00');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
